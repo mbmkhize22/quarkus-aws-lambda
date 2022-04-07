e06f8e701cff66bf5fbf946b9d55f41595e0a6eb
Nhlanhla Mkhize <mbmkhize22@gmail.com>
2022-04-07 19:05:56 +0200

    lambda can read props

--- src/main/java/com/nhlanhla/quarkusawslambda/GreetingResource.java
+++ src/main/java/com/nhlanhla/quarkusawslambda/GreetingResource.java
@@ -1,13 +1,24 @@
 package com.nhlanhla.quarkusawslambda;
 
+import org.eclipse.microprofile.config.inject.ConfigProperties;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+@ApplicationScoped
 @Path("/hello")
 public class GreetingResource {
+
+
+    @Inject
+    @ConfigProperty(name = "message", defaultValue = "Failed to read property file")
+    private String message;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -20,5 +31,12 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello(@PathParam("name")String name) {
         return "Hello RESTEasy " + name;
+    }
+
+    @GET
+    @Path("/message")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloMessage() {
+        return "Hello RESTEasy " + message;
     }
 }
